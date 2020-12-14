bc4e9a809268c4af0675956ab6837d5239b708bf
Martin <~~~you@example.com>
2020-12-14 15:51:02 -0300

    crear_admin_v3

--- django_email_detector/app_email_detector/management/commands/createsu.py
+++ django_email_detector/app_email_detector/management/commands/createsu.py
@@ -4,4 +4,4 @@ from django.core.management.base import BaseCommand
 class Command(BaseCommand):
     def handle(self, *args, **options):
         if not User.objects.filter(username="admin").exists():
-            User.objects.create_superuser("admin", "admin@admin.com", "admin123")
+            User.objects.create_superuser("admin", "admin@admin.com", "admin_lemon123")
