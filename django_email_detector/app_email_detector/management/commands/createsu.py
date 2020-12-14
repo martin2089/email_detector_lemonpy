3ad5fd727ef682bd4f74df2e605f2025b0d9b6a4
Martin <~~~you@example.com>
2020-12-14 20:34:06 -0300

    lunes_v6

--- django_email_detector/app_email_detector/management/commands/createsu.py
+++ django_email_detector/app_email_detector/management/commands/createsu.py
@@ -7,10 +7,10 @@ class Command(BaseCommand):
             User.objects.create_superuser("admin", "admin@admin.com", "admin_lemon123")
         
         if not User.objects.filter(username="user_small_quota").exists():
-            User.objects.create_user("user_small_quota", "sm4ll_1234")
+            User.objects.create_user("user_small_quota","user_small_quota@lemon.com","sm4ll_1234")
         
         if not User.objects.filter(username="user_big_quota").exists():
-            User.objects.create_user("user_big_quota", "b1gg_1234")
+            User.objects.create_user("user_big_quota","user_big_quota@lemon.com","b1gg_1234")
         
         if not User.objects.filter(username="dashboard").exists():
-            User.objects.create_user("dashboard", "d4shb0ard123")
+            User.objects.create_user("dashboard","dashboard@lemon.com", "d4shb0ard123")
