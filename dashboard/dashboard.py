e303fd3751376e5497bbdbf17a4c7820e03694ac
ncocaro <74457652+ncocaro@users.noreply.github.com>
2020-12-18 18:08:43 -0300

    correccion dashboard filtros avanzados

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -15,6 +15,7 @@ from string import punctuation
 import time
 from pathlib import Path
 import os, json, requests, pytz
+from streamlit import caching
 
 st.set_page_config(layout="wide")
 
@@ -49,66 +50,18 @@ st.image(img,width=150)
 st.title("Dashboard Monitoreo API clasificacion de Spam-Ham")
 
 st.sidebar.title("Filtros y segmentación de datos")
+if st.sidebar.button ("Actualizar base de datos"):
+    caching.clear_cache()
+    df = get_base()
+    st.balloons()
+
 size = st.sidebar.slider("Cantidad de consultas a visualizar en la tabla",1,len(df))
 
-#---------Filtro avanzado---------
-if st.sidebar.checkbox("Busqueda avanzada"):
-    
-    options = np.sort(df.ID.unique()).tolist()
-    select = st.sidebar.multiselect('Seleccione el/los usuarios',options,default=options)
-    df_filter = df[df['ID'].isin(select)]
+#---------Graficos Dashboard---------
 
-    start_date = st.sidebar.date_input('Seleccione la fecha desde', df['Fecha'].min())
-    end_date = st.sidebar.date_input('Seleccione la fecha hasta', df['Fecha'].max() + timedelta(days=1))
-
-    if start_date < end_date:
-        mask = (df_filter['Fecha'] >= start_date) & (df_filter['Fecha'] <= end_date)
-        dias = end_date - start_date
-        st.sidebar.success('Ha seleccionado `%s` dias' % (dias.days))
-    else:
-        st.sidebar.error('Error: Debe seleccionar al menos un dia posterior a la fecha inicial.')
-
-    df_filter = df_filter.loc[mask]
-
-    mail = st.sidebar.radio("Clasificación de mails",("Todos","SPAM","HAM"))
-
-    if mail != "Todos":
-        df_filter = df_filter[df_filter['Resultado']==mail]
-
-    st.header('Gráficos segmentados por filtros')
-
-    #Graficos segmentados
-    fig, (ax1, ax2) = plt.subplots(1, 2,figsize=(15, 5),sharex=False, sharey=False)
-    fig.suptitle('Actividad')
-
-    df_spam = df_filter.groupby(['ID','Resultado']).size().reset_index(name='qty')
-    sns.barplot(ax=ax1,data=df_spam,x=df_spam.ID,y=df_spam.qty,hue=df_spam.Resultado, palette="Blues")
-    ax1.set_title('Total clasificaciones por usuario')
-    ax1.set_xlabel('Usuario')
-    ax1.set_ylabel('Consultas')
-    ax1.tick_params(axis='x',rotation=0,labelsize=8)
-
-    df_counts = df_filter.groupby(['Fecha', 'ID']).size().reset_index(name='counts')
-    df_counts['cum_count'] = df_counts.groupby('ID')['counts'].cumsum()
-    sns.lineplot(ax=ax2,x=df_counts.Fecha,y=df_counts.cum_count,hue=df_counts.ID,data=df_counts,palette="Blues", marker='o')
-    ax2.set_title('Acumulado de consultas')
-    ax2.set_xlabel('Fecha')
-    ax2.set_ylabel('Consultas acumuladas')    
-    ax2.tick_params(axis='x',rotation=0,labelsize=8)
-
-    st.pyplot(fig)
-
-    st.header('Base de datos segmentada por filtros')
-
-    #Base de datos
-    df_filter = df_filter[:size]
-    st.table(df_filter.astype('object'))
-
-else:
-    st.header("Resumen registro de actividad histórico")
+def graficos_totales1(df):
     days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
     hours = np.arange(0,24)
-     
     df['Dia_sem'] = pd.Categorical(df.Dia_sem, categories=days, ordered=True)
     df_consultas = df.groupby(['Dia_sem','Hora'])['Resultado'].count()
     df_consultas = pd.DataFrame(df_consultas).reset_index()
@@ -135,9 +88,12 @@ else:
     ax2.set_title('Serie de tiempo de uso de la API',size=12)
     ax2.set_ylabel('Cantidad de consultas diarias')
     ax2.set_xlabel('Fecha')
+    ax2.set_xlim(min(df_uso['Fecha']),max(df_uso['Fecha']))
     ax2.tick_params(axis='x',rotation=45)
 
-    st.pyplot(fig)
+    return fig
+
+def graficos_totales2(df):
 
     fig, (ax3,ax4) = plt.subplots(1,2,figsize=(15, 5))
 
@@ -153,13 +109,84 @@ else:
     ax3.spines['bottom'].set_visible(False)
 
     #Histograma de Longitud de los mails
-    sns.histplot(ax=ax4, data=df, x="Longitud",hue="Resultado", kde=True)
+    sns.histplot(ax=ax4, data=df, x="Longitud",hue="Resultado")#, kde=True)
     ax4.set_title('Histograma de longitud de los textos según clasificación',size=12)
     ax4.set_xlabel('Cantidad de palabras')
     ax4.set_ylabel('Cantidad de consultas')
 
-    st.pyplot(fig)
+    return fig
 
+def graficos_seg(df_filter):
+    #Graficos segmentados
+    fig, (ax1, ax2) = plt.subplots(1, 2,figsize=(15, 5),sharex=False, sharey=False)
+    fig.suptitle('Actividad')
+
+    #Grafico de barras por usuario
+    df_spam = df_filter.groupby(['ID','Resultado']).size().reset_index(name='qty')
+    sns.barplot(ax=ax1,data=df_spam,x=df_spam.ID,y=df_spam.qty,hue=df_spam.Resultado, palette="Blues")
+    ax1.set_title('Total clasificaciones por usuario')
+    ax1.set_xlabel('Usuario')
+    ax1.set_ylabel('Consultas')
+    ax1.tick_params(axis='x',rotation=0,labelsize=8)
+
+    #Grafico de lineas de actividad
+    df_counts = df_filter.groupby(['Fecha', 'ID']).size().reset_index(name='counts')
+    df_counts['cum_count'] = df_counts.groupby('ID')['counts'].cumsum()
+    sns.lineplot(ax=ax2,x=df_counts.Fecha,y=df_counts.cum_count,hue=df_counts.ID,data=df_counts,palette="Blues", marker='o')
+    ax2.set_title('Acumulado de consultas')
+    ax2.set_xlabel('Fecha')
+    ax2.set_ylabel('Consultas acumuladas')    
+    ax2.tick_params(axis='x',rotation=0,labelsize=8)
+
+    return fig
+
+#---------Main Dashboard---------
+
+if st.sidebar.checkbox("Busqueda avanzada"):
+
+    st.header('Gráficos segmentados por filtros')    
+    options = np.sort(df.ID.unique()).tolist()
+    select = st.sidebar.multiselect('Seleccione el/los usuarios',options,default=options)
+    df_filter = df[df['ID'].isin(select)]
+    start_date = st.sidebar.date_input('Seleccione la fecha desde', df['Fecha'].min())
+    end_date = st.sidebar.date_input('Seleccione la fecha hasta', df['Fecha'].max() + timedelta(days=1))
+    mail = st.sidebar.radio("Clasificación de mails",("Todos","SPAM","HAM"))
+
+    if start_date < end_date:
+        mask = (df_filter['Fecha'] >= start_date) & (df_filter['Fecha'] <= end_date)
+        dias = end_date - start_date
+        st.sidebar.success('Ha seleccionado `%s` dias' % (dias.days))
+        df_filter = df_filter.loc[mask]
+        
+        if mail != "Todos":
+            df_filter = df_filter[df_filter['Resultado']==mail]
+            if len(df_filter)>0:
+                st.pyplot(graficos_seg(df_filter))
+                #Base de datos
+                df_filter = df_filter[:size]
+                st.table(df_filter.astype('object'))
+            else:
+                st.warning('No hay resultados en la base de datos que responden a los criterios seleccionados')
+        else:        
+            if len(df_filter)>0:
+                st.pyplot(graficos_seg(df_filter))
+                #Base de datos
+                df_filter = df_filter[:size]
+                st.table(df_filter.astype('object'))
+            else:
+                st.warning('No hay resultados en la base de datos.')
+    else:
+        st.warning('Error: Debe seleccionar al menos un dia posterior a la fecha inicial.')
+else:
+
+    st.header("Resumen total de registros de actividad histórico")
+    st.pyplot(graficos_totales1(df))
+    st.pyplot(graficos_totales2(df))
     #Base de datos
     df = df[:size]
-    st.table(df.astype('object'))
+    if len(df)>0:
+        st.table(df.astype('object'))
+    else:
+        st.warning('No hay resultados en la base de datos')
+
+    
